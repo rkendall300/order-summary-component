4fd4049bf3cf5ce5ed82706530ccb225d98dd7ba
Ryan Kendall <ryekendallion@gmail.com>
2024-01-18 21:49:07 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import musicIcon from './images/icon-music.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div id="backdrop">
+        <div id="card">
+          <div id="fm-image-container">
+            <div id="fm-image"></div>
+          </div>
+          <div id="info">
+            <h1 id="title">Order Summary</h1>
+            <p id="description">You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
+            <div id="plan-container">
+              <img id="music-logo" src={musicIcon} alt="music"></img>
+              <div id="plan-info">
+                <p id="annual">Annual Plan</p>
+                <p id="price">$59.99/year</p>
+              </div>
+              <a id="change" href="javascript:;">Change</a>
+            </div>
+            <div id="footer">
+              <button id="checkout">Proceed to Payment</button>
+              <p id="cancel">Cancel Order</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
